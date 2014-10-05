7453dc999bd2617d18d4b1e6a17c8127c67beb2e
baxnie <henrique_santiago93@hotmail.com>
2014-10-05 13:10:51 +0000

    Gitignore, main activity

--- gen/com/wingeon/truco/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.wingeon.truco;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/wingeon/truco/MainActivity.java
+++ src/com/wingeon/truco/MainActivity.java
@@ -1,15 +1,48 @@
 package com.wingeon.truco;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	private Button m_newGameButton;
+	private Button m_connectButton;
+	private Button m_optionsButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		m_newGameButton = (Button)findViewById(R.id.new_game);
+		m_newGameButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// it was the 1st button
+			}
+		});
+		
+		m_connectButton = (Button)findViewById(R.id.connect);
+		m_connectButton.setEnabled(false);
+		m_connectButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// it was the 1st button
+			}
+		});
+		
+		m_optionsButton = (Button)findViewById(R.id.options);
+		m_optionsButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, OptionsActivity.class);
+			    startActivity(intent);
+				// it was the 1st button
+			}
+		});
 	}
 }
