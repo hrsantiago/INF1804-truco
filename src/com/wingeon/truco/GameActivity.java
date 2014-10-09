bb123cfafe8af788118e221fa113abd022644cb2
Henrique Santiago <henrique_santiago93@hotmail.com>
2014-10-09 10:28:32 -0300

    OptionsMenu, implicit intent

--- src/com/wingeon/truco/GameActivity.java
+++ src/com/wingeon/truco/GameActivity.java
@@ -1,10 +1,16 @@
 package com.wingeon.truco;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class GameActivity extends Activity {
 
+	private static String HELP_WEBSITE = "http://www.jogatina.com/regras-como-jogar-truco.html";
 	private Game m_game = new Game();
 	
 	@Override
@@ -23,5 +29,30 @@ public class GameActivity extends Activity {
 	protected void onStop() {
 		super.onStop();
 		m_game.stop();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.game, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case R.id.new_game:
+			recreate();
+			return true;
+		case R.id.help:
+			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(HELP_WEBSITE));
+			startActivity(intent);
+			return true;
+		case R.id.exit:
+			finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
