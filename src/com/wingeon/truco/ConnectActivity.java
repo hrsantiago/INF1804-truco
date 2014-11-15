2e11c7fff3e7cb4f9d8f2ad5c91451bde2ef9c4f
baxnie <henrique_santiago93@hotmail.com>
2014-11-15 19:00:17 -0200

    Device discoverability

--- src/com/wingeon/truco/ConnectActivity.java
+++ src/com/wingeon/truco/ConnectActivity.java
@@ -1,14 +1,89 @@
 package com.wingeon.truco;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class ConnectActivity extends Activity {
+	
+	private Button m_refreshButton;
+	private ListView m_listView;
+	private ArrayAdapter<String> m_arrayAdapter;
+	private BluetoothAdapter m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+	
+	private final BroadcastReceiver m_receiver = new BroadcastReceiver() {
+	    public void onReceive(Context context, Intent intent) {
+	        String action = intent.getAction();
+	        if(BluetoothDevice.ACTION_FOUND.equals(action)) {
+	            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+	            m_arrayAdapter.add(device.getName() + "\n" + device.getAddress());
+	        }
+	    }
+	};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_connect);
+		
+		m_arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
 
+		m_refreshButton = (Button)findViewById(R.id.refresh_bt);
+		m_refreshButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				m_bluetoothAdapter.cancelDiscovery();
+				m_arrayAdapter.clear();
+				m_bluetoothAdapter.startDiscovery();
+			}
+		});
+		
+		m_listView = (ListView)findViewById(R.id.devices_bt);
+		m_listView.setAdapter(m_arrayAdapter);
+		
+		m_listView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            	m_bluetoothAdapter.cancelDiscovery();
+            	//String itemValue = (String)m_listView.getItemAtPosition(position);
+            	//String id = itemValue;
+            }
+
+       }); 
+		
+		IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+		registerReceiver(m_receiver, filter);
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		unregisterReceiver(m_receiver);
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		m_arrayAdapter.clear();
+		m_bluetoothAdapter.startDiscovery();
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+		m_bluetoothAdapter.cancelDiscovery();
 	}
 }
--- src/com/wingeon/truco/MainActivity.java
+++ src/com/wingeon/truco/MainActivity.java
@@ -15,7 +15,8 @@ import android.widget.Button;
 public class MainActivity extends Activity
 {
 	static int REQUEST_ENABLE_BT_CONNECT = 1;
-	static int REQUEST_ENABLE_BT_HOST = 2;
+	static int REQUEST_ENABLE_BT_DISCOVER_HOST = 2;
+	static int DISCOVERABLE_DURATION = 300;
 
 	private Button m_newGameButton;
 	private Button m_connectButton;
@@ -23,12 +24,12 @@ public class MainActivity extends Activity
 	private Button m_statisticsButton;
 	private Button m_optionsButton;
 	private BluetoothAdapter m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		m_newGameButton = (Button)findViewById(R.id.new_game);
 		m_newGameButton.setOnClickListener(new OnClickListener() {
 			@Override
@@ -37,86 +38,78 @@ public class MainActivity extends Activity
 				startActivity(intent);
 			}
 		});
-		
+
 		m_connectButton = (Button)findViewById(R.id.connect);
 		m_connectButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if(!m_bluetoothAdapter.isEnabled()) {
-				    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-				    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT_CONNECT);
-				}
-				else {
-					Intent intent = new Intent(MainActivity.this, ConnectActivity.class);
-				    startActivity(intent);
-				}
+				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT_CONNECT);
 			}
 		});
-		
+
 		m_hostButton = (Button)findViewById(R.id.host);
 		m_hostButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if(!m_bluetoothAdapter.isEnabled()) {
-				    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-				    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT_HOST);
-				}
-				else {
-					Intent intent = new Intent(MainActivity.this, RoomActivity.class);
-				    startActivity(intent);
-				}
+				Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+				discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVERABLE_DURATION);
+				startActivityForResult(discoverableIntent, REQUEST_ENABLE_BT_DISCOVER_HOST);
 			}
 		});
-		
+
 		m_statisticsButton = (Button)findViewById(R.id.statistics);
 		m_statisticsButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(MainActivity.this, StatisticsActivity.class);
-			    startActivity(intent);
+				startActivity(intent);
 			}
 		});
-		
+
 		m_optionsButton = (Button)findViewById(R.id.options);
 		m_optionsButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(MainActivity.this, OptionsActivity.class);
-			    startActivity(intent);
+				startActivity(intent);
 			}
 		});
-		
+
 		if(m_bluetoothAdapter == null) {
-		    m_connectButton.setEnabled(false);
-		    m_hostButton.setEnabled(false);
+			m_connectButton.setEnabled(false);
+			m_hostButton.setEnabled(false);
 		}
 
 		// Code to improve testing
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
-	            WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
-	            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
-	            WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+				WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+				WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+				WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-		
+
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-		int color = Color.parseColor(prefs.getString("table_color", getResources().getString(R.color.table_green)));
+		String defaultColor = getResources().getString(R.color.table_green);
+		int color = Color.parseColor(prefs.getString("table_color", defaultColor));
 		findViewById(R.id.main_view).setBackgroundColor(color);
 	}
-	
+
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if(resultCode == Activity.RESULT_OK) {
-			if(requestCode == REQUEST_ENABLE_BT_CONNECT) {
+		if(requestCode == REQUEST_ENABLE_BT_CONNECT) {
+			if(resultCode == RESULT_OK) {
 				Intent intent = new Intent(MainActivity.this, ConnectActivity.class);
-			    startActivity(intent);
+				startActivity(intent);
 			}
-			else if(requestCode == REQUEST_ENABLE_BT_HOST) {
+		}
+		else if(requestCode == REQUEST_ENABLE_BT_DISCOVER_HOST) {
+			if(resultCode != RESULT_CANCELED) {
 				Intent intent = new Intent(MainActivity.this, RoomActivity.class);
-			    startActivity(intent);
+				startActivity(intent);
 			}
 		}
 	}
